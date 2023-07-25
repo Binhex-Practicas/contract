f54e690d4726d83c976a3785150c4287fc13e44c
Albert Mendez <j.mendez@binhex.es>
2023-07-25 08:05:24 +0000

    [16.0][MIG] agreement_account: Migration to 16.0

--- agreement_account/__manifest__.py
+++ agreement_account/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Agreement Account",
     "summary": "Agreement on invoices",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Contract",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
